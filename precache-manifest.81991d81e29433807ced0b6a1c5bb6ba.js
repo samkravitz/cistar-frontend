ce9380d212156f82b0c6b34ea0d6b314df046979
Sam Kravitz <samkravitz99@gmail.com>
2020-03-05 21:49:40 -0500

    Updates

---
+++ precache-manifest.81991d81e29433807ced0b6a1c5bb6ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76db9652567cefa9d9bf92e82d40afae",
+    "url": "/cistar-frontend/index.html"
+  },
+  {
+    "revision": "7d401e4126504259dd67",
+    "url": "/cistar-frontend/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "7439f6e7c9fc41d6cf6a",
+    "url": "/cistar-frontend/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "7d401e4126504259dd67",
+    "url": "/cistar-frontend/static/js/2.624ca591.chunk.js"
+  },
+  {
+    "revision": "7439f6e7c9fc41d6cf6a",
+    "url": "/cistar-frontend/static/js/main.e771d14e.chunk.js"
+  },
+  {
+    "revision": "5952009413c65c76ba0c",
+    "url": "/cistar-frontend/static/js/runtime~main.cd80e71c.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/cistar-frontend/precache-manifest.81991d81e29433807ced0b6a1c5bb6ba.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cistar-frontend/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
