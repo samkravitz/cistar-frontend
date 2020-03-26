7797c8bd63178e365f885c59bcb9a0d184400c7c
Sam Kravitz <samkravitz99@gmail.com>
2020-03-26 00:39:01 -0400

    Cleanup App

--- src/App.js
+++ src/App.js
@@ -1,50 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react'
 import Main from './Main'
 
-class App extends Component {
-    constructor(props) {
-        super(props);
-
-        //this.toggle = this.toggle.bind(this);
-        
-        //this.r2p2 = this.r2p2.bind(this);
-        //this.sr1 = this.sr1.bind(this);
-
-
-        this.state = {
-            isOpen: false,
-            n_r : 2,
-            n_p : 2,
-            fr1: '',
-            r1: ['','','']
-            
-        };
-        
-    }
-changef(event) {
-  this.setState({fr1:event.target.value.replace("C:\\fakepath\\", "")})
-  
-}
-
-sr1(event){
-	const url = "http://localhost:5000/pdf/";
-	fetch(url + this.state.fr1).then(response => response.json()).then(data => this.setState({ r1: data.a }));
-}    
-
-toggle() {
-   this.setState({
-      isOpen: !this.state.isOpen
-   });
-}
-    render() { 
-        return (
-            <div className="App">
-                <Main />
-            </div> 
-        )
-    }
-
-
+const App = () => {
+    return (
+        <div className="App">
+            <Main />
+        </div> 
+    )
 }
 
 export default App
