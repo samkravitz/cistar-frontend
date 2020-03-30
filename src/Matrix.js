85e42fa3afae3f07ba422d2022073c70441feca8
Sam Kravitz <samkravitz99@gmail.com>
2020-03-30 16:36:44 -0400

    Made my own table lol

--- src/Matrix.js
+++ src/Matrix.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { useTable } from 'react-table'
 
 const styles = {
     main: {
@@ -7,7 +6,7 @@ const styles = {
         width: '100vw',
         display: 'flex',
         justifyContent: 'center',
-        
+
     },
 
     table: {
@@ -32,63 +31,50 @@ const styles = {
 }
 
 const Matrix = ({ matrix }) => {
-    const columns = React.useMemo(
-        () => [
-            { Header: 'Name', accessor: 'name'},
-            { Header: 'Flammability', accessor: 'flammability' },
-            { Header: 'Reactivity', accessor: 'reactivity' },
-            { Header: 'Skin absorption', accessor: 'skinAbsorption' },
-            { Header: 'Skin contact', accessor: 'skinContact' },
-            { Header: 'Eye contact', accessor: 'eyeContact' },
-            { Header: 'Respiratory', accessor: 'respiratory' },
-            { Header: 'Carcinogen', accessor: 'carcinogen' },
-            { Header: 'Reproductive hazard', accessor: 'reproductiveHazard' },
-            { Header: 'Sensitizer', accessor: 'sensitizer' },
-            { Header: 'Other', accessor: 'other' },
-            { Header: 'Ingestion', accessor: 'ingestion' }
-        ],
-        []
-    )
-
-    const data = React.useMemo(() => [...matrix], [matrix])
-
-    console.log(data)
-
-    // Use the state and functions returned from useTable to build your UI
-    const {
-        getTableProps,
-        getTableBodyProps,
-        headerGroups,
-        rows,
-        prepareRow,
-    } = useTable({
-        columns,
-        data,
-    })
+    const columns = [
+        'Name',
+        'Flammability',
+        'Reactivity',
+        'Skin absorption',
+        'Skin contact',
+        'Eye contact',
+        'Respiratory',
+        'Carcinogen',
+        'Reproductive hazard',
+        'Sensitizer',
+        'Other',
+        'Ingestion',
+    ]
 
     return (
         <div className='Matrix' style={styles.main}>
-            <table style={styles.table} {...getTableProps()}>
-                <thead style={styles.th}>
-                    {headerGroups.map(headerGroup => (
-                        <tr {...headerGroup.getHeaderGroupProps()}>
-                            {headerGroup.headers.map(column => (
-                                <th style={styles.th} {...column.getHeaderProps()}>{column.render('Header')}</th>
-                            ))}
-                        </tr>
-                    ))}
+            <table style={styles.table}>
+                <thead>
+                    <tr>
+                        {columns.map((column, i) => (
+                            <th style={styles.th} key={i}>{column}</th>
+                        ))}
+                    </tr>
                 </thead>
-                <tbody {...getTableBodyProps()}>
-                    {rows.map((row, i) => {
-                        prepareRow(row)
-                        return (
-                            <tr {...row.getRowProps()}>
-                                {row.cells.map(cell => {
-                                    return <td style={{ ...styles.th, color: cell.value, backgroundColor: cell.value }} {...cell.getCellProps()}>{cell.render('Cell')}</td>
-                                })}
-                            </tr>
-                        )
-                    })}
+
+                <tbody>
+                    {matrix.map((row, i) => (
+                        <tr key={i}>
+                            <td style={{ ...styles.th, }}>{row.name}</td>
+                            <td style={{ ...styles.th, color: row.flammability, backgroundColor: row.flammability }}>{row.flammability}</td>
+                            <td style={{ ...styles.th, color: row.reactivity, backgroundColor: row.reactivity }}>{row.reactivity}</td>
+                            <td style={{ ...styles.th, color: row.skinAbsorption, backgroundColor: row.skinAbsorption }}>{row.skinAbsorption}</td>
+                            <td style={{ ...styles.th, color: row.skinContact, backgroundColor: row.skinContact }}>{row.skinContact}</td>
+                            <td style={{ ...styles.th, color: row.eyeContact, backgroundColor: row.eyeContact }}>{row.eyeContact}</td>
+                            <td style={{ ...styles.th, color: row.respiratory, backgroundColor: row.respiratory }}>{row.respiratory}</td>
+                            <td style={{ ...styles.th, color: row.carcinogen, backgroundColor: row.carcinogen }}>{row.carcinogen}</td>
+                            <td style={{ ...styles.th, color: row.reproductiveHazard, backgroundColor: row.reproductiveHazard }}>{row.reproductiveHazard}</td>
+                            <td style={{ ...styles.th, color: row.sensitizer, backgroundColor: row.sensitizer }}>{row.sensitizer}</td>
+                            <td style={{ ...styles.th, color: row.other, backgroundColor: row.other }}>{row.other}</td>
+                            <td style={{ ...styles.th, color: row.ingestion, backgroundColor: row.ingestion }}>{row.ingestion}</td>
+                        </tr>
+
+                    ))}
                 </tbody>
             </table>
         </div>
