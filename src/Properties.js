a1a2a7cff57028463eec60a9a277630ea06edea9
Sam Kravitz <samkravitz99@gmail.com>
2020-04-01 10:17:13 -0400

    Thermal Conductivity Units Lowercase 'k'

--- src/Properties.js
+++ src/Properties.js
@@ -28,7 +28,7 @@ const Properties = props => {
             <h6>Auto ignition temperature (°C)</h6>
             <h6>Decomposition temperature (°C)</h6>
             <h6>Viscosity ()</h6>
-            <h6>Thermal Conductivity - K</h6>
+            <h6>Thermal Conductivity - k</h6>
             <h6 style={{ color: '#c71e1e' }}>Specific heat capacity - Cp</h6>
         </div>
 
