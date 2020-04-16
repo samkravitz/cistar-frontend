a35310abb4f3771a6a8ff858e33bf6aa7126104e
Sam Kravitz <samkravitz99@gmail.com>
2020-04-15 20:00:40 -0400

    Turn Body to a function

--- src/body/Body.js
+++ src/body/Body.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Button } from 'reactstrap'
 import actions from '../redux/actions'
 import { connect } from 'react-redux'
@@ -8,67 +8,52 @@ import Product from './Product'
 import Properties from './Properties'
 import Diluent from './Diluent'
 
-class Body extends Component {
-
-    constructor(props) {
-        super(props)
-        this.state = {
-            numberOfElements: props.numReactants + props.numProducts + props.numDiluents + 1,
-            reactants: {
-                1: {
-
+const Body = ({ numReactants, numProducts, numDiluents }) => {
+    return (
+        <div className="Body">
+            <div style={style}>
+                <Properties />
+                {/* this is just a hacky way to map numReactants number of times: */}
+                {
+                    [...Array(numReactants)].map((e, i) => (
+                        <Reactant
+                            key={i}
+                            index={i}
+                            number={i + 1}
+                        />
+                    ))
                 }
-            }
-        }
-    }
-
-    render() {
-        const { numReactants, numProducts, numDiluents } = this.props
-        return (
-            <div className="Body">
-                <div style={style}>
-                    <Properties numberOfElements={this.state.numberOfElements} />
-                    {/* this is just a hacky way to map numReactants number of times: */}
-                    {
-                        [...Array(numReactants)].map((e, i) => (
-                            <Reactant
-                                key={i}
-                                index={i}
-                                number={i + 1}
-                            />
-                        ))
-                    }
-                    {
-                        [...Array(numProducts)].map((e, i) => (
-                            <Product
-                                key={i}
-                                index={i}
-                                number={i + 1}
-                            />
-                        ))
-                    }
-                    {
-                        [...Array(numDiluents)].map((e, i) => (
-                            <Diluent
-                                key={i}
-                                index={i}
-                                number={i + 1}
-                            />
-                        ))
-                    }
-                </div>
-                <div style={style}>
-                    <Button
-                        color="primary"
-                        onClick={() => this.props.calculate(this.state.reactants)}
-                    >
-                        Calculate
-                </Button>
-                </div>
+                {
+                    [...Array(numProducts)].map((e, i) => (
+                        <Product
+                            key={i}
+                            index={i}
+                            number={i + 1}
+                        />
+                    ))
+                }
+                {
+                    [...Array(numDiluents)].map((e, i) => (
+                        <Diluent
+                            key={i}
+                            index={i}
+                            number={i + 1}
+                        />
+                    ))
+                }
             </div>
+            <div style={style}>
+                <Button
+                    color="primary"
+                    onClick={() => this.props.calculate(this.state.reactants)}
+                >
+                    Calculate
+                </Button>
+            </div>
+        </div>
 
-        )
-    }
+    )
+
 }
 
 const style = {
