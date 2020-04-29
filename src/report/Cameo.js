f10823eb700f6db4b9802255b62593ed38816699
Sam Kravitz <samkravitz99@gmail.com>
2020-04-29 13:29:53 -0400

    Import color pics explicitly

--- src/report/Cameo.js
+++ src/report/Cameo.js
@@ -3,8 +3,16 @@ import React from 'react'
 import { connect } from 'react-redux'
 import './cameo.css'
 
+import green from './images/reactivity/green_compatible.png'
+import yellow from './images/reactivity/yellow_caution.png'
+import red from './images/reactivity/red_caution.png'
+
 const Cameo = ({ cameoTable }) => {
     console.log(cameoTable)
+
+    console.log(green)
+    console.log(yellow)
+    console.log(red)
     return (
         <div className='Cameo'>
             <caption>Cameo Matrix</caption>
