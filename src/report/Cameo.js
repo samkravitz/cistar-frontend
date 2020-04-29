025351eedf18cb25517a9c380a43ffa813fadb7b
Sam Kravitz <samkravitz99@gmail.com>
2020-04-29 13:51:02 -0400

    Find reason yellow image wasn't loading

--- src/report/Cameo.js
+++ src/report/Cameo.js
@@ -8,12 +8,8 @@ import yellow from '../images/reactivity/yellow_caution.png'
 import red from '../images/reactivity/red_incompatible.png'
 
 const Cameo = ({ cameoTable }) => {
-    console.log(green)
-    console.log(yellow)
-    console.log(red)
-
     cameoTable = cameoTable.split('/images/reactivity/green_compatible.png').join(green)
-    cameoTable = cameoTable.split('/images/reactivity/yellow_caution').join(yellow)
+    cameoTable = cameoTable.split('/images/reactivity/yellow_caution.png').join(yellow)
     cameoTable = cameoTable.split('/images/reactivity/red_incompatible.png').join(red)
 
     console.log(cameoTable)
