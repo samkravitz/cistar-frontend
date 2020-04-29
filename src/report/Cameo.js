d57f68e5cd1dc303d9a01b8f9a6b16474c4b6f0b
Sam Kravitz <samkravitz99@gmail.com>
2020-04-29 13:37:59 -0400

    replace images

--- src/report/Cameo.js
+++ src/report/Cameo.js
@@ -5,14 +5,18 @@ import './cameo.css'
 
 import green from '../images/reactivity/green_compatible.png'
 import yellow from '../images/reactivity/yellow_caution.png'
-import red from '../images/reactivity/red_caution.png'
+import red from '../images/reactivity/red_incompatible.png'
 
 const Cameo = ({ cameoTable }) => {
-    console.log(cameoTable)
-
     console.log(green)
     console.log(yellow)
     console.log(red)
+
+    cameoTable = cameoTable.replace('/images/reactivity/green_compatible.png', green)
+    cameoTable = cameoTable.replace('/images/reactivity/yellow_caution', yellow)
+    cameoTable = cameoTable.replace('/images/reactivity/red_incompatible.png', red)
+
+    console.log(cameoTable)
     return (
         <div className='Cameo'>
             <caption>Cameo Matrix</caption>
