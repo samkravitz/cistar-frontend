03a2210cca1cc32b1ad09d825052476ed37a96f8
Sam Kravitz <samkravitz99@gmail.com>
2020-05-05 10:54:37 -0400

    Return null for cameo table if it fails

--- src/report/Cameo.js
+++ src/report/Cameo.js
@@ -7,21 +7,24 @@ import green from '../images/reactivity/green_compatible.png'
 import yellow from '../images/reactivity/yellow_caution.png'
 import red from '../images/reactivity/red_incompatible.png'
 
-const Cameo = ({ cameoTable }) => {
+const Cameo = ({ cameoTable, cameoTableComplete }) => {
     cameoTable = cameoTable.split('/images/reactivity/green_compatible.png').join(green)
     cameoTable = cameoTable.split('/images/reactivity/yellow_caution.png').join(yellow)
     cameoTable = cameoTable.split('/images/reactivity/red_incompatible.png').join(red)
 
-    return (
+    return cameoTableComplete ? 
+    (
         <div className='Cameo'>
             <caption>Cameo Matrix</caption>
             <div dangerouslySetInnerHTML={{ __html: cameoTable }}></div>
         </div>
-    )
+    ) :
+    null
 }
 
 const mapStateToProps = state => ({
     cameoTable: state.report.cameoTable,
+    cameoTableComplete: state.status.cameoTableComplete,
 })
 
 export default connect(mapStateToProps)(Cameo)
