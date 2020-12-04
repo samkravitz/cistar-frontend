e0355946ab7e72233a12609649073a73a739ff7c
Sam Kravitz <samkravitz99@gmail.com>
2020-12-03 18:49:26 -0600

    Updates

---
+++ precache-manifest.1cc9bee9a88aaabc4867930c8b712491.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cadc39c1bf8b5cb12ce712fdb683a1fa",
+    "url": "/cistar-frontend/index.html"
+  },
+  {
+    "revision": "dcec7e1619c3e30c6188",
+    "url": "/cistar-frontend/static/css/3.2a86faee.chunk.css"
+  },
+  {
+    "revision": "756561a92eaf1e1800dd",
+    "url": "/cistar-frontend/static/css/main.c2097976.chunk.css"
+  },
+  {
+    "revision": "c5c0fa78f4bce28e5021",
+    "url": "/cistar-frontend/static/js/2.5c000c6d.chunk.js"
+  },
+  {
+    "revision": "dcec7e1619c3e30c6188",
+    "url": "/cistar-frontend/static/js/3.093aabb2.chunk.js"
+  },
+  {
+    "revision": "ce2eb6e25125381cafec",
+    "url": "/cistar-frontend/static/js/4.48aa4d16.chunk.js"
+  },
+  {
+    "revision": "756561a92eaf1e1800dd",
+    "url": "/cistar-frontend/static/js/main.a134a81d.chunk.js"
+  },
+  {
+    "revision": "1ceac914ffb76e547bb9",
+    "url": "/cistar-frontend/static/js/runtime~main.68e485d5.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/cistar-frontend/precache-manifest.2dbea261d68277812020f8b2012853f2.js"
+  "/cistar-frontend/precache-manifest.1cc9bee9a88aaabc4867930c8b712491.js"
 );
 
 self.addEventListener('message', (event) => {
